47cee52e549dc1ad88fbb072537655a7308f55f0
juaneth <euanwalker2008@gmail.com>
2023-01-29 22:09:24 +0000

    Revert "deploy"
    This reverts commit f38341942f646b68d270efd91a56d0a92fe12e62.

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: '../build',
-        base: '/NuRem/'
+        outDir: '../build'
     }
 })
