51ad086804ed4ebb7d2d2454fb39f314250bf702
juaneth <euanwalker2008@gmail.com>
2023-01-30 21:36:53 +0000

    scalable screen size fixes

--- web/src/Routes/Home.jsx
+++ web/src/Routes/Home.jsx
@@ -14,8 +14,8 @@ const auth = getAuth();
 function Home() {
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-col lg:flex-row w-full transition-all border-b-2">
-        <div className="p-8 bg-neutral/50 transition-all w-80">
+      <div className="flex flex-col lg:flex-row w-full transition-all">
+        <div className="border-b border-primary p-8 bg-neutral/50 transition-all lg:w-80">
           <h1 className="text-2xl text-primary">NuRem</h1>
 
           <p className="opacity-75">NuRem Developer Tools</p>
@@ -24,20 +24,20 @@ function Home() {
         <div className="flex-grow flex flex-row transition-all hover:z-50">
           <Link
             to="/IdeaGen"
-            className="p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50"
+            className="border-b-2 p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50"
           >
             <h1 className="text-xl text-accent">IdeaGen</h1>
 
             <p className="opacity-75">Generate app ideas using AI</p>
           </Link>
 
-          <div className="p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50">
+          <div className="border-b-2 p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50">
             <h1 className="text-xl text-accent">Workbook</h1>
 
             <p className="opacity-75">Keep track of your projects</p>
           </div>
 
-          <div className="p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50">
+          <div className="border-b-2 p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50">
             <h1 className="text-xl text-accent">Build tools</h1>
 
             <p className="opacity-75">
@@ -48,7 +48,7 @@ function Home() {
       </div>
 
       <div className="flex flex-col lg:flex-row w-full transition-all">
-        <div className="p-8 bg-neutral/50 transition-all w-80">
+        <div className="border-b border-primary p-8 bg-neutral/50 transition-all lg:w-80">
           <h1 className="text-2xl text-primary">NuRem Apps</h1>
 
           <p className="opacity-75">NuRem Apps</p>
@@ -57,7 +57,7 @@ function Home() {
         <div className="flex-grow flex flex-row transition-all hover:z-50">
           <Link
             to="/IdeaGen"
-            className="p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50"
+            className="border-b-2 p-8 bg-neutral/80 basis-1/3 transition-all hover:bg-black hover:shadow-lg hover:-translate-y-5 hover:z-50"
           >
             <h1 className="text-xl text-accent">Omnipetal</h1>
 
